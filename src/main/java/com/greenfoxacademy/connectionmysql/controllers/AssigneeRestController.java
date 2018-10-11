71401fae9d801deee30a00f618430ad2bd28aa9e
Peter <wrkpeter@gmail.com>
2018-10-11 11:31:50 +0200

    feat(security): done, need to know why...

---
+++ src/main/java/com/greenfoxacademy/connectionmysql/controllers/AssigneeRestController.java
@@ -0,0 +1,22 @@
+package com.greenfoxacademy.connectionmysql.controllers;
+
+import com.greenfoxacademy.connectionmysql.services.AssigneeServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AssigneeRestController {
+
+  private AssigneeServices assigneeServices;
+
+  @Autowired
+  public AssigneeRestController(AssigneeServices assigneeServices) {
+    this.assigneeServices = assigneeServices;
+  }
+
+  @GetMapping("/todo/api/assignees")
+  public Object listOfAssignees() {
+    return assigneeServices.findAllAssignees();
+  }
+}
